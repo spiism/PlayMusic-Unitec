e47fc86962e2da91d070ad41e02d771bdba9f610
Vina <spiism@gmail.com>
2019-09-27 07:02:17 +1200

    Initial Commit

---
+++ PlayMusic/ViewController2.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController2.swift
+//  PlayMusic
+//
+//  Created by Vina on 20/09/2019.
+//  Copyright © 2019 Vina. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController2: UIViewController {
+    var audioPlayer = AVAudioPlayer()
+    var isPlaying = false
+    var timer:Timer!
+    
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var playedTime: UILabel!
+    
+    @IBAction func playOrPauseMusic(_ sender: Any) {
+        if isPlaying {
+            audioPlayer.pause()
+            isPlaying = false
+        } else {
+            audioPlayer.play()
+            isPlaying = true
+            
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func stopMusic(_ sender: Any) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        isPlaying = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        trackTitle.text = "Hello - Adele"
+        let path = Bundle.main.path(forResource: "hello - adele", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer =  try AVAudioPlayer(contentsOf: url)
+        } catch {
+            // can't load file
+        }
+    }
+    
+    @objc func updateTime() {
+        let currentTime = Int(audioPlayer.currentTime)
+        let minutes = currentTime/60
+        let seconds = currentTime - minutes * 60
+        
+        playedTime.text = String(format: "%02d:%02d", minutes,seconds) as String
+    }
+
+}
+
+
